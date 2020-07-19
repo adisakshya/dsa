fd196d915dc193959d3f44fa93b9c68affd9e4b3
root <root@DESKTOP-A7R7503.localdomain>
2020-07-19 17:43:13 +0530

    added /practise

---
+++ C++/practise/data_structure/stacks/design_&_implementation/queue_using_stacks.cpp
@@ -0,0 +1,77 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class StackQueue {
+    private:
+        stack<int> s1;
+        stack<int> s2;
+    public:
+        void enqueue(int B);
+        int dequeue();
+        int get_front();
+        int get_rear();
+};
+
+void StackQueue :: enqueue(int x)
+ {
+    
+    while(!s1.empty()) {
+        int top = s1.top();
+        s2.push(top);
+        s1.pop();
+    }
+    
+    s1.push(x);
+    while(!s2.empty()) {
+        int top = s2.top();
+        s1.push(top);
+        s2.pop();
+    }
+}
+
+int StackQueue :: dequeue()
+{
+    
+    if(s1.empty()) {
+        return INT_MIN;
+    }
+    
+    int front = s1.top();
+    s1.pop();
+    return front;
+}
+
+int StackQueue :: get_front()
+{
+    
+    if(s1.empty()) {
+        return INT_MIN;
+    }
+    
+    int front = s1.top();
+    return front;
+}
+
+int StackQueue :: get_rear()
+{
+    
+    if(s1.empty()) {
+        return INT_MIN;
+    }
+    
+    int rear = -1;
+    while(!s1.empty()) {
+        int top = s1.top();
+        rear = top;
+        s2.push(top);
+        s1.pop();
+    }
+    
+    while(!s2.empty()) {
+        int top = s2.top();
+        s1.push(top);
+        s2.pop();
+    }
+
+    return rear;
+}
