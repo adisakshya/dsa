87664eb48eaf9fa9ef9a2eebd629abb4667067fd
root <root@DESKTOP-A7R7503.localdomain>
2020-07-19 17:51:25 +0530

    added fun-recursion

---
+++ C++/algorithms/recursion/fun.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void fun(int num) {
+    if(num < 1) {
+        return;
+    }
+    cout << num << " ";
+    fun(num - 1);
+    cout << num << " ";
+}
+
+int main() {
+    int num = 3;
+    cout << "Doing fun" << endl;
+    fun(num);
+}
